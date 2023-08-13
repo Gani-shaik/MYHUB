04c48e4fb8991edeedbef0ac0749d03b035b5bfc
Gani-shaik <shaikganishid@gmail.com>
2023-08-13 23:51:05 +0530

    Game

---
+++ game.py
@@ -0,0 +1,9 @@
+print("Tresure box")
+print("find the tresure box")
+print("you have 3 chances to get tresure")
+print("Start")
+TresureBox=int(input("choose the number"))
+if ( TresureBox == 5 ) or (TresureBox == 2) or (TresureBox == 7):
+  print("WIN")
+else:
+  print("Try again")
