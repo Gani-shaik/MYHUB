e0280fd5a42eb4876a3873a0a56e0dc6c11b7b38
Gani-shaik <shaikganishid@gmail.com>
2023-08-16 21:37:41 +0530

    class, id and javascript practice

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 print("Tresure box")
 print("find the tresure box")
-print("you have 3 chances to get tresure")
-print("Start")
-TresureBox=int(input("choose the number"))
+print("you have 3 chances to get the tresure box")
+print("Start the game ")
+TresureBox=int(input("choose the number =  "))
 if ( TresureBox == 5 ) or (TresureBox == 2) or (TresureBox == 7):
   print("WIN")
 else:
